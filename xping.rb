33c2bfb18e880d28795754b120a47e0e53fa4859
Martin Topholm <mph@hoth.dk>
2015-12-22 14:19:06 +0100

    homebrew: xping 1.3.1 formula

--- xping.rb
+++ xping.rb
@@ -4,19 +4,8 @@ class Xping < Formula
   depends_on "libevent"
 
   stable do
-    url "https://github.com/martintopholm/xping/archive/v1.3.tar.gz"
-    sha256 "6bab7abf5433fd1daa7de939727db95f88a2ac3630f92ec2f55cfae9d69846e5"
-
-    # patches waiting for upstream release tag
-    patch do
-      url "http://martin.topholm.eu/pub/xping/brew/0001-main-keep-env-cflags-and-clear-install-deps.patch"
-      sha256 "50455e4d6ca14f8e7264bcc5b5b71226f149e082e2944901cec3958cac7249d6"
-    end
-
-    patch do
-      url "http://martin.topholm.eu/pub/xping/brew/0003-termio-correct-labelwidth-variable-scope.patch"
-      sha256 "dc3a0b7ad9f0031fead8f41cd59ed338bb701ef1615985f84388b0587229189c"
-    end
+    url "https://github.com/martintopholm/xping/archive/v1.3.1.tar.gz"
+    sha256 "0d2ce17ab3d2d6b205c2bb003552e1112b028ba79c2c2cdd067383828cc939cc"
   end
 
   head do
